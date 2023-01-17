82178601b9dcef5632b799dd8d0f74b9fe559a00
nesquikm <drapolyuk@gmail.com>
2023-01-17 17:56:29 +0400

    Clean up the code

--- packages/nekoton_bridge/native/src/api.rs
+++ packages/nekoton_bridge/native/src/api.rs
@@ -1,22 +1,6 @@
 #![allow(unused_variables)]
 
-// use std::fmt::Debug;
-// use std::ops::Deref;
-// use std::sync::atomic::{AtomicI32, Ordering};
-// use std::sync::Arc;
-// pub use std::sync::{Mutex, RwLock};
-// use std::thread::sleep;
-// use std::time::Duration;
-
-// use anyhow::{anyhow, Result};
-
 use flutter_rust_bridge::*;
-// use lazy_static::lazy_static;
-
-// use crate::data::{EnumAlias, Id, MyEnum, MyStruct, StructAlias, UserIdAlias};
-// pub use crate::data::{FrbOpaqueReturn, FrbOpaqueSyncReturn, HideData, NonSendHideData};
-// use crate::new_module_system::{use_new_module_system, NewSimpleStruct};
-// use crate::old_module_system::{use_old_module_system, OldSimpleStruct};
 
 pub fn simple_adder_sync(a: i32, b: i32) -> SyncReturn<i32> {
   SyncReturn(a + b)
