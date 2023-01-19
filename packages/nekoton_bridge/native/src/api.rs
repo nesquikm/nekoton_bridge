247ecf13c0b07c82ad330ac9855154fad3b9d546
nesquikm <drapolyuk@gmail.com>
2023-01-19 13:37:48 +0400

    Fix rust format and integration tests

--- packages/nekoton_bridge/native/src/api.rs
+++ packages/nekoton_bridge/native/src/api.rs
@@ -3,34 +3,32 @@
 use flutter_rust_bridge::*;
 
 pub fn simple_adder_sync(a: i32, b: i32) -> SyncReturn<i32> {
-  SyncReturn(a + b)
+    SyncReturn(a + b)
 }
 
 pub fn simple_adder(a: i32, b: i32) -> i32 {
-  a + b
+    a + b
 }
 
 pub trait MyFormat {
-  fn my_format(&self) -> String;
+    fn my_format(&self) -> String;
 }
 
 impl MyFormat for i32 {
-  fn my_format(&self) -> String {
-      return format!("i32 {}", self)
-  }
+    fn my_format(&self) -> String {
+        return format!("i32 {}", self);
+    }
 }
 
 pub struct MyClass {
-  pub val: i32
+    pub val: i32,
 }
 
 impl MyClass {
-  pub fn new(a: i32) -> MyClass {
-    MyClass {
-      val: a
+    pub fn new(a: i32) -> MyClass {
+        MyClass { val: a }
     }
-  }
-  pub fn my_format(&self) -> String {
-    return self.val.my_format();
-  }
+    pub fn my_format(&self) -> String {
+        return self.val.my_format();
+    }
 }
